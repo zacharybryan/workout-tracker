3a2f4ef589c0b68e149173d66d5ebe9ee845d76d
Zach Bryan <zacharybryan@icloud.com>
2020-10-30 12:37:37 -0700

    connected to mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-// const logger = require('morgan');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 8080;
 
@@ -10,6 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+
+// routes //
 
 app.listen(PORT, function() {
     console.log('listening on port ' + PORT);
