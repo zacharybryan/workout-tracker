defbcb1a5495a94b6f105f65528d53eac4a29668
Zach Bryan <zacharybryan@icloud.com>
2020-10-30 12:57:09 -0700

    created workout schema to record new workouts based of template from seeder file

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 // routes //
 
 app.use(require("./routes/view.js"));
-app.use(require("./routes/api.js"));
+// app.use(require("./routes/api.js"));
 
 app.listen(PORT, function() {
     console.log('listening on port ' + PORT);
