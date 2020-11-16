ebe82513233610ba4361c542134e5b576a877e80
Zach Bryan <zacharybryan@icloud.com>
2020-11-16 15:22:21 -0800

    intergrated mongoose

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 // requires 
 const router = require("express").Router();
+const mongoose = require("mongoose");
 const Workout = require("../models/workout");
 
 // creates workouts
@@ -13,6 +14,23 @@ router.post("/api/workouts", (req, res) => {
         });
 });
 
+// find by id
+router.put("/api/workouts/:id", (req, res) => {
+    // use workout to find update object
+    Workout.update({_id:mongoose.Types.ObjectId(req.params.id)},{
+        // imports info to exercise
+        $push:{exercises:req.body}
+    })
+    // to json
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    // errors
+    .catch(err => {
+        res.json(err);
+    });
+})
+
 // finds previous workouts 
 router.get("/api/workouts", (req, res) => {
     Workout.find()
@@ -23,5 +41,16 @@ router.get("/api/workouts", (req, res) => {
             res.json(err);
         });
 });
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find()
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+// add api/range find all work outs and return them ---> like get workouts 
 
 module.exports = router;
